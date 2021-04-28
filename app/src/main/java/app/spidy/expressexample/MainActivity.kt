9f3577a4b2e81b88cc872cb15f93586183f1c329
anyms <mytellee@gmail.com>
2021-04-29 03:28:17 +0530

    adding stream as response

--- app/src/main/java/app/spidy/expressexample/MainActivity.kt
+++ app/src/main/java/app/spidy/expressexample/MainActivity.kt
@@ -3,6 +3,8 @@ package app.spidy.expressexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import app.spidy.express.Express
+import app.spidy.hiper.Hiper
+import app.spidy.kotlinutils.debug
 import kotlinx.android.synthetic.main.activity_main.*
 import java.net.Inet4Address
 import java.net.NetworkInterface
@@ -13,13 +15,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val hiper = Hiper.getInstance()
+
         var express: Express? = null
         startServer.setOnClickListener {
             val host = getIpv4HostAddress()
+            debug(host)
             express?.terminate()
-            express = Express(host = host)
+            express = Express(host = host, port = 3000)
             express?.get("/") { req, res ->
                 res.send("hello, world")
+            }
+            express?.get("/mp3") { req, res ->
+                val r = hiper.get("http://www.friendstamilmp3.in/songs2/A-Z%20Movie%20Songs/Antha%20Veetil%20Oru%20Kovil/Aayiram%20Jenmangal.mp3", isStream = true)
+                res.stream(r.stream!!, r.headers["content-length"]!!.toLong(), "audio/mp3")
+                r.stream?.close()
             }
             express?.runAsync()
             startServer.text = host
--- express/src/main/java/app/spidy/express/data/Response.kt
+++ express/src/main/java/app/spidy/express/data/Response.kt
@@ -1,6 +1,8 @@
 package app.spidy.express.data
 
 import app.spidy.express.StatusCode
+import java.io.InputStream
+import java.io.PrintStream
 import java.io.PrintWriter
 import java.net.Socket
 
@@ -19,6 +21,27 @@ class Response(private val client: Socket) {
         printWriter.print(resString)
         printWriter.flush()
     }
+
+    fun stream(stream: InputStream, contentLength: Long, contentType: String) {
+        addHeader("Content-Type", contentType)
+        val resString = "HTTP/1.1 $statusCode ${StatusCode.codes[statusCode]}\r\nConnection: close\r\nServer: Express/1.0\r\n${headerString}Content-Length: ${contentLength}\r\n\r\n"
+        val printWriter = PrintStream(client.getOutputStream())
+        printWriter.write(resString.toByteArray())
+        printWriter.flush()
+        val buf = ByteArray(8192)
+        var length: Int
+        while (stream.read(buf).also { length = it } > 0) {
+            printWriter.write(buf, 0, length)
+        }
+        printWriter.flush()
+    }
+
+//    fun streamTest(s: String) {
+//        val resString = "HTTP/1.1 $statusCode ${StatusCode.codes[statusCode]}\r\nConnection: close\r\nServer: Express/1.0\r\n${headerString}Content-Length: ${s.length}\r\n\r\n$s"
+//        val printWriter = PrintStream(client.getOutputStream())
+//        printWriter.write(resString.toByteArray())
+//        printWriter.flush()
+//    }
 
     fun json(s: String) {
         addHeader("Content-Type", "application/json")
